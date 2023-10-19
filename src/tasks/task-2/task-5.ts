e0d61bd4960f91da8950f5467224518992a1d095
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-10-19 18:21:35 +0500

    Fixed the 5th task

--- src/tasks/task-2/task-5.ts
+++ src/tasks/task-2/task-5.ts
@@ -1,18 +1,9 @@
 // Задание 5
 function areEqual(obj1: object, obj2: object): boolean {
-    let arrayFromObject1 = Object.keys(obj1)
-    let arrayFromObject2 = Object.keys(obj2)
-
-    // Сравниваем по длине
-    if (arrayFromObject1.length === arrayFromObject2.length) {
-      // Находим все одинаковые элементы
-      let items = arrayFromObject1.filter(item => obj1[item as keyof object] === obj2[item as keyof object])
-      
-      // Если все элементы равны, то true
-      if (items.length === arrayFromObject1.length) {
-        return true
-      }
+    if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+      return true
     }
+
     return false
 }
 
