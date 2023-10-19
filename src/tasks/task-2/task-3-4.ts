3c7379824e1c6c39ff49d9d78ede6f8a8b691d31
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-10-19 18:55:25 +0500

    Modified task-3-4

--- src/tasks/task-2/task-3-4.ts
+++ src/tasks/task-2/task-3-4.ts
@@ -1,15 +1,11 @@
 // Задание 3 - 4
 function capitalizesFirstLetter(sentence: string): string {
-    let arrayWords: string[] = sentence.split(' ');
-
-    // Находим количесво букв в первом слове
-    let countSymbolsInFirstWord: number = arrayWords[0].length
-
-    // Проверяем есть ли слово под этим индексом
-    if (arrayWords[countSymbolsInFirstWord] !== undefined) {
-        arrayWords.splice(countSymbolsInFirstWord, 1);
-    }
-    return arrayWords.map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+    const words: string[] = sentence.split(' ');
+    const firstWordLength: number = words[0].length;
+  
+    return words.filter((word, index) => index !== firstWordLength)
+                .map((word) => word[0].toUpperCase() + word.slice(1))
+                .join(' ');
 }
 
 console.log(capitalizesFirstLetter('Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили.'))
